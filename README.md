169d809e1ee7efbb0260994942d203017bba87df
Gordon Williams <gw@pur3.co.uk>
2017-08-03 10:50:01 +0100

    update with tiny waterer js

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 Tomato Feeder Web Bluetooth interface
 =====================================
 
+* [Espruino JS code for Minimal Waterer](tiny_waterer.js)
+* [Full Espruino JS code for Waterer](waterer.js)
+* [Source code for dashboard](index.html)
+
+See it in action on https://gfwilliams.github.io/MakeTomatoes
+
+------
+
 Using Puck.js: http://www.puck-js.com
 
 Using TinyDash from https://github.com/espruino/TinyDash
 
 Originally made for a Make: magazine article - http://makezine.com/
 
-See it in action on https://gfwilliams.github.io/MakeTomatoes
+
---
+++ tiny_waterer.js
@@ -0,0 +1,31 @@
+// Very short plant waterer software for Make: article
+
+E.setTimeZone(-8 /* PST */);
+var hadWater = false;
+
+function waterPlants(water, feed) {
+  digitalPulse(D1, 1, water*1000);
+  if (feed)
+    digitalPulse(D2, 1, feed*1000);
+}
+
+// Check watering every 10 minutes
+function checkWater() {
+  var now = new Date();
+  var h = now.getHours();
+  var day = now.getDay(); // day of week
+  if (h==8 || h==19) {
+    // feed in the morning on mon, weds, fri
+    var doFeed = h==8 && (day==1 || day==3 || day==5);
+    if (!hadWater) waterPlants(300, doFeed?30:0);
+    hadWater = true;
+  } else {
+    hadWater = false;
+  }
+}
+setInterval(checkWater, 10*60000);
+
+// When a button is pressed, water for 30 sec
+setWatch(function() {
+  waterPlants(30,0);
+}, BTN, {edge:"rising",debounce:50,repeat:true});
